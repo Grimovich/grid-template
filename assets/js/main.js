73c834b0c68a8f49a264e8808fb7b573d2eff821
Nichita Grimovici <125678372+Grimovich@users.noreply.github.com>
2023-04-04 20:24:50 +0300

    last update:)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-	$('.header-navbar__btn').on('click',function(){
-		$(this).toggleClass('-active');
-    $('.header-navbar__list').stop(true, true).slideToggle(500);
-	});
+$(document).ready(() => {
+  $('.header-navbar__btn').on('click', function () {
+    $(this).toggleClass('-active');
+    $('.header-navbar__list').toggleClass('-active');
+  });
 });
